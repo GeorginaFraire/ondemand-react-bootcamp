d893fc4ce802285554aa5b1f486434a8874acf21
Georgina Fraire Miranda <georgina.fraire@wizeline.com>
2022-06-27 18:53:38 -0500

    paginationControls.css added

--- src/Components/Pagination-Controls/pagination-controls.jsx
+++ src/Components/Pagination-Controls/pagination-controls.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-//import "./pagination-controls.css"
+import "./PaginationControls.css"
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faAngleRight} from "@fortawesome/free-solid-svg-icons"
 import {faAngleLeft} from "@fortawesome/free-solid-svg-icons"
@@ -7,13 +7,13 @@ import {faAngleLeft} from "@fortawesome/free-solid-svg-icons"
 
 function PaginationControls(){
     return (
-        <div style={{float:'right', marginTop:'10px', marginRight:'30px'}}>
-            <button style={{margin:'2px', backgroundColor:'transparent', color:'#1e517b', border: '1px solid #1e517b', cursor:'pointer'}}>
+        <div className="container-pg-controls">
+            <button className="btn-pg-controls moves-pg-controls">
                 <FontAwesomeIcon icon={faAngleLeft}></FontAwesomeIcon>
                 
             </button>
-            <button style={{margin:'2px', backgroundColor:'transparent', color:'#1e517b', border: 'none'}}>x</button>
-            <button style={{margin:'2px', backgroundColor:'transparent', color:'#1e517b', border: '1px solid #1e517b',cursor:'pointer'}}>
+            <button className="btn-pg-controls">x</button>
+            <button className="btn-pg-controls moves-pg-controls">
                 <FontAwesomeIcon icon={faAngleRight}></FontAwesomeIcon>
             </button>
         </div>
