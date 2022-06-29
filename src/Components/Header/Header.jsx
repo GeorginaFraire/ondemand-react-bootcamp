780930737aff83f2618ebc2c8a11bb22e7a31e26
Georgina Fraire Miranda <georgina.fraire@wizeline.com>
2022-06-29 10:43:39 -0500

    Change Class Component To Function Component

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,26 +1,25 @@
 import React from "react";
 import HeaderStyle from "./HeaderStyled";
-import logo from "../../images/logo-ecommerce.png"
+import logo from "../../images/logo-ecommerce.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
 
-export default class Header extends React.Component {
-  render() {
-    return (
-      <HeaderStyle>
-        <div className="header-container ">
-          <div className="header-logo">
-            <img src={logo} alt="e-commerce" onClick={this.props.GoHome} />
-          </div>
-          <div className="header-search">
-            <input type="text" placeholder="Search" />
-          </div>
-
-          <div className="header-cart">
-            <FontAwesomeIcon icon={faCartShopping}  size="2x"/>
-          </div>
+function Header({GoHome}) {
+  return (
+    <HeaderStyle>
+      <div className="header-container ">
+        <div className="header-logo">
+          <img src={logo} alt="e-commerce" onClick={GoHome} />
         </div>
-      </HeaderStyle>
-    );
-  } 
-} 
+        <div className="header-search">
+          <input type="text" placeholder="Search" />
+        </div>
+
+        <div className="header-cart">
+          <FontAwesomeIcon icon={faCartShopping} size="2x" />
+        </div>
+      </div>
+    </HeaderStyle>
+  );
+}
+export default Header;
